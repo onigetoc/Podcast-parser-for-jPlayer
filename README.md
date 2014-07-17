906dc35f53802957364f7bd0f792a1cd300e90f1
Gino <ginocote@gmail.com>
2014-07-17 11:52:19 -0400

    First upload

---
+++ README.md
@@ -0,0 +1,61 @@
+Podcast parser for jPlayer
+========
+
+Podcast parser for jPlayer
+
+=================
+
++ Play all podcast with media: ogg, mp3, m4a, m4v, mp4, 
++ Show podcast poster.
++ You can load any playlist with jQuery.
+
+Demo
+-----------
++ Demo: http://scripts.toolurl.com/audio/Podcast-parser-for-jPlayer/demo.html
+
+How to use
+---------------
+
+just add jQuery and JQHand js file to your project.
+```
+loadPodcast('http://your-podcast-url.xml')
+```
+More way to load podcast width jQuery in the demo file.
+
+Quick start
+-----------
+
+Clone the repo, `git clone git://github.com/onigetoc/Podcast-parser-for-jPlayer.git`.
+
+
+Bug tracker
+-----------
+
+Have a bug? Please create an issue here on GitHub.
+
+https://github.com/onigetoc/Podcast-parser-for-jPlayer/issues
+
+
+Contributing
+------------
+
+Please submit all pull requests against *-wip branches. If your unit test contains javascript patches or features, you must include relevant unit tests. Thanks!
+
+
+Authors
+-------
+
+**Gino Cote**
+
++ http://twitter.com/ginocote
++ http://github.com/onigetoc
+
+
+
+Copyright and license
+---------------------
+
+
+The MIT License is simple and easy to understand and it places almost no restrictions on what you can do with Podcast parser for jPlayer.
+
+  http://opensource.org/licenses/mit-license.php
---
+++ assets/podparser.php
@@ -0,0 +1,60 @@
+<?php
+//header('Access-Control-Allow-Origin: *');
+
+$url = $_GET["Podurl"];
+
+	$pod->pod = @file_get_contents($url);
+	/*if(!$this->pod = @file_get_contents($url))
+		{
+		throw new Exception('Can\'t load podcast XML file');
+		}*/
+	$pod->pod = str_replace('itunes:', '', $pod->pod);
+	$pod->pod = simplexml_load_string($pod->pod);
+	
+
+//$pod = str_replace('itunes:', '', $pod);
+
+//$image = $pod->channel['itunes:summary']->image['url']; //GC chanel image
+//$title = $pod->channel->title; // podcast title
+$main_poster = $pod->pod->channel->image['href'];
+if(!$main_poster) 
+$main_poster = $pod->pod->channel->image->url;
+//echo $main_poster;
+
+// get main chanel author
+$main_author = $pod->pod->channel->author;
+
+$items = $pod->pod->channel->item;
+
+$i = 0;
+$data = array();
+foreach($items as $item) {  //loop over elements you want to return
+	
+	$datum = parse_url($item->enclosure['url']);
+	$parts = pathinfo($datum['path']);
+	$type = $parts['extension'];
+	
+	if($type=="mp4") $type = "m4v";
+	if($type=="ogg") $type = "oga";
+	//audio/mp3	
+	
+	//Get item poster
+	$poster = $item->image['href'];
+	if(!$poster) $poster = $main_poster;
+	
+	//Get item author
+	$author = $item->author;
+	if(!$author) $author = $main_author;
+	
+	$data[] = array(
+        'title' => (string)$item->title,
+        $type => (string)$item->enclosure['url'],//replace this with the XML elements you want to get
+		'poster' => (string)$poster,
+		'artist' => (string)$author,
+    );
+	if (++$i == 20) break;// change this to the number of elements you want to get
+}
+
+$jsdata = ($_GET['callback'].'('.json_encode($data).');');
+echo htmlspecialchars($jsdata, ENT_NOQUOTES, 'utf-8'); // return JSON wrapped in callback
+?>
