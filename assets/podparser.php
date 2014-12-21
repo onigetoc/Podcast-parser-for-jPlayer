5b180f51eb5d194931b5e3f3f9a05c1ad1fef734
Gino <ginocote@gmail.com>
2014-12-21 11:51:11 -0500

    Order podcast by date
    Order podcast by date

--- assets/podparser.php
+++ assets/podparser.php
@@ -8,6 +8,7 @@ $url = $_GET["Podurl"];
 		{
 		throw new Exception('Can\'t load podcast XML file');
 		}*/
+		
 	$pod->pod = str_replace('itunes:', '', $pod->pod);
 	$pod->pod = simplexml_load_string($pod->pod);
 
@@ -22,6 +23,7 @@ $main_poster = $pod->pod->channel->image->url;
 $main_author = $pod->pod->channel->author;
 
 $items = $pod->pod->channel->item;
+
 
 $i = 0;
 $data = array();
@@ -43,14 +45,20 @@ foreach($items as $item) {  //loop over elements you want to return
 	$author = $item->author;
 	if(!$author) $author = $main_author;
 	
+	$tmp = str_replace(" ","",$item->pubDate);
 	$data[] = array(
         'title' => (string)$item->title,
         $type => (string)$item->enclosure['url'],//replace this with the XML elements you want to get
+		'download' => (string)$item->enclosure['url'],
+		//'poster' => (string)$res->image['href'],
 		'poster' => (string)$poster,
 		'artist' => (string)$author,
     );
 	if (++$i == 20) break;// change this to the number of elements you want to get
 }
+
+array_multisort($data,SORT_DESC); //Order by date
+// Order by date help: http://stackoverflow.com/questions/20662389/is-there-a-way-to-sort-by-pubdate-in-descending-order
 
 $jsdata = ($_GET['callback'].'('.json_encode($data).');');
 echo htmlspecialchars($jsdata, ENT_NOQUOTES, 'utf-8'); // return JSON wrapped in callback
